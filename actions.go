1eb23fa4f0e40398c2fdc7d513249f6ac6a849ef
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2020-03-06 11:53:06 +0800

    Update actions.go

--- actions.go
+++ actions.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"runtime"
 	nonce "github.com/LarryBattle/nonce-golang"
+	gpv "github.com/go-playground/validator/v10"
 )
 
 func Demo() {
+	gpv.New()
 	fmt.Printf("Nonce token: %s\n", nonce.NewToken())
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Printf("GOOS: %s\n", runtime.GOOS)
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module dummy.module/actions
 
 go 1.13
+
+require (
+	github.com/LarryBattle/nonce-golang v0.0.0-20150303194802-a7d5615a19fd
+	github.com/go-playground/validator/v10 v10.2.0
+)
